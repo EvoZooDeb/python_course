62e62784c89634784f353cd70b3f3f10830d5c4f
Kornel Ecsedi <ecsedi@unideb.hu>
2022-04-27 13:53:12 +0200

    Modified blockgen.py.

--- blockgen.py
+++ blockgen.py
@@ -2,22 +2,69 @@
 
 import re
 import sys
+import json
 
-def digest(block):
-  print(block)
+def clean(block):
+    result = []
+    for line in block:
+        line = line.strip()
+        line = re.sub(r'^[0-9]+\s*', '', line)
+        if re.fullmatch(r'^[0-9]+$', line):
+            continue
+        if line:
+            result.append(line)
+    return result
 
 def blocks(f):
     blocklines = []
     for line in f:
-        if re.match(r'[0-9]+\s+[0-9]+', line):
+        if re.match(r'[0-9]', line):
             if blocklines:
-                yield blocklines
+                yield clean(blocklines)
             blocklines = [ line ]
         else:
             if blocklines:
                 blocklines.append(line)
     if blocklines:
-        yield blocklines
+        yield clean(blocklines)
+
+def digest(block):
+
+    # Eurytop|Stenotop|Ubiquist|Synanthrop|Coprophil|eurytop|Kaltstenotop
+    # "":  xxx - xxx - xxx REGEXP: '^([a-zA-Z]+ — )+[a-zA-Z]+$'
+
+    info        = {}
+    firstline   = True
+    label       = ''
+    info[label] = ''
+
+    for line in block:
+
+        if firstline:
+            spid, spname   = line.split(' ', 1)
+            info['spid'  ] = spid
+            info['spname'] = spname
+            firstline      = False
+        else:
+            if re.match(r'H:', line):
+                label       = 'H:'
+                info[label] = ''
+            elif re.match(r'Na:', line):
+                label       = 'Na:'
+                info[label] = ''
+            elif re.match(r'Ni:', line):
+                label       = 'Ni:'
+                info[label] = ''
+            info[label] += ' '+line
+
+    found = re.search(r' ([a-zA-Z]+ [—-] )+[a-zA-Z]+$', info[''])
+    if found:
+      print(found.start(), info[''])
+    #print(json.dumps(info, indent=2, ensure_ascii=False))
+    #print('')
 
 for block in blocks(sys.stdin):
+#    for i in block:
+#        print(i)
+#    print('')
     digest(block)
